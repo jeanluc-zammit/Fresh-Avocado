ba8a46e4eb265866e4a9d057a90154e2c08be0ee
Jeanluc Zammit <jeanpowerson@Jeanlucs-MBP.lan>
2018-05-29 19:54:22 +0200

    Set up Project
    Set up terrain and began coding the movements

---
+++ Assets/Code/Player.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour {
+
+	private Rigidbody2D rb2d;
+	public float offset;
+	public float moveForce = 365f;
+	public float maxSpeed = 5f;
+	public float jumpForce = 1000f;
+	public Transform groundCheck;
+	public Text Battery_Count;
+
+
+	// Use this for initialization
+	void Awake () {
+
+		rb2d = this.GetComponent<Rigidbody2D> ();
+
+	}
+
+	void Start()
+	{
+		/*
+		float x = Camera.main.ViewportToWorldPoint (Vector3.zero).x;
+		Vector3 pos = transform.position;
+		pos.x = x + offset;
+		transform.position = pos;
+		*/
+		//the text which will appear on screen
+
+
+
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey (KeyCode.UpArrow)) {
+
+			rb2d.velocity = new Vector2 (0f, 5f);
+
+		} else if (Input.GetKey (KeyCode.DownArrow)) {
+
+			rb2d.velocity = new Vector2 (0f, -5f);
+
+		} else if (Input.GetKey (KeyCode.LeftArrow)) {
+
+			rb2d.velocity = new Vector2 (-5f, 0f);
+
+		} else if (Input.GetKey (KeyCode.RightArrow)) {
+
+			rb2d.velocity = new Vector2 (5f, 0f);
+
+		} else if (Input.GetKey (KeyCode.Space)) {
+
+			rb2d.AddForce (Vector2.up * jumpForce);
+
+		} else {
+
+			rb2d.velocity = new Vector2(0f, 0f);
+		}
+	}
+}
