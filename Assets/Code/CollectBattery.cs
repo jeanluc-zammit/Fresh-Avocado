b881b4739d41cd78e3f6a9ac5a58a3708dc32272
Jeanluc Zammit <jeanpowerson@Jeanlucs-MacBook-Pro.local>
2018-05-31 10:23:15 +0200

    Update 1

---
+++ Assets/Code/CollectBattery.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CollectBattery : MonoBehaviour {
+
+	public Text BatteryCount;
+
+	public int CountBattery;
+
+	// Use this for initialization
+	void Start () {
+		CountBattery = 0;
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter2D (Collider2D Player) {
+
+		if (Player.tag == "battery") {
+
+			CountBattery ++;
+
+			Debug.Log (CountBattery);
+
+		}
+	}
+}
